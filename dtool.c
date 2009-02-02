cbbe3760610de10f49b9d9b9f9fb47f4b4421127
lunatic.fp <lunatic.fp@77be1d12-e5d5-11dd-9ba5-89a75009fd5d>
2009-02-02 07:20:24 +0000

    Created exec()

--- dtool.c
+++ dtool.c
@@ -1,32 +1,47 @@
 /*
-###################################################################
-#                                                                 #
-#  Author: Marcel Ribeiro Dantas <vuln at slackware-rn.com.br>    #
-#  Website: http://www.slackware-rn.com.br/~vuln                  #
-#  This code is licensed under GNU GPLv3 and any other            #
-#  version of this license.                                       #
-#                                                                 #
-#  A copy of this license can be read in:                         #
-#  http://www.gnu.org/licenses/gpl-3.0.txt                        #
-#                                                                 #
-#  dtool - a dictionary tool with many features.                  #
-#  A tool for help you translating                                #
-#  and learning more about a word in                              #
-#  a lot of different languages                                   #
-#                                                                 #
-################################################################### */
+####################################################################
+#                                                                  #
+#  Author:  Marcel Ribeiro Dantas <vuln at slackware-rn.com.br>    #
+#  Website: http://www.slackware-rn.com.br/~vuln                   #
+#  This code is licensed under GNU GPLv3 and any other             #
+#  version of this license.                                        #
+#                                                                  #
+#  A copy of this license can be read in:                          #
+#  http://www.gnu.org/licenses/gpl-3.0.txt                         #
+#                                                                  #  
+#  dtool - a dictionary tool with many features.                   # 
+#  A tool for help you translating                                 # 
+#  and learning more about a word in                               #  
+#  a lot of different languages                                    #
+#                                                                  #
+#################################################################### */
 
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+#ifdef __linux__
+#include <getopt.h>
+#endif
 #include "config.h"
 
-int main(int argc, char **argv, char **envp) {
-	
-	char command[256] = { 0 };
+void exec(const char *fmt, ...)
+{
+    char cmd[8192] = { 0 };
+    va_list l;
 
+    va_start(l, fmt);
+    /* XXX: I'd use vasprintf, but... gah... */
+    vsnprintf(cmd, 8192, fmt, l);
+    va_end(l);
+    /* XXX: I'll implement something with vfork + execve later */
+    system(cmd);
+    return ;
+}
+
+int main(int argc, char **argv, char **envp) {
 	if (argc == 2 && (!(strcmp(argv[1],"-h")))) {
 		printf("Usage: dtool [option] [from-language] [to-language] \"word to find or translate\"\n");
 		printf("-d        dictionary option.\n");
@@ -39,14 +54,10 @@ int main(int argc, char **argv, char **envp) {
     }
 	
 	else if (argc == 3 && (!(strcmp(argv[1],"-p")))) {
-		snprintf(command, sizeof(command), "wget -qc http://www.google.com/dictionary/sounds/%s.mp3", argv[2]);
-		system(command);
-		command[0] = '\0';
-		snprintf(command, sizeof(command), "xdg-open %s.mp3 &> /dev/null", argv[2]);
-		system(command);
-		command[0] = '\0';
+		exec("wget -qc http://www.google.com/dictionary/sounds/%s.mp3", argv[2]);
+		exec("xdg-open %s.mp3 &> /dev/null", argv[2]);
 #ifndef KEEP_FILES
-		snprintf(command, sizeof(command), "rm -f %s.mp3", argv[2]);
+		exec("rm -f %s.mp3", argv[2]);
 #endif
     }
 
@@ -56,14 +67,14 @@ int main(int argc, char **argv, char **envp) {
 	    exit(1);
     }
 		
-	else if (!(strcmp(argv[1],"-d"))) 
-		snprintf(command, sizeof(command), "xdg-open \"http://www.google.com/dictionary?aq=f&langpair=%s%%7C%s&q=%s&hl=en\"", argv[2], argv[3], argv[4]);
+	else if (!(strcmp(argv[1],"-d")))
+        exec("xdg-open \"http://www.google.com/dictionary?aq=f&langpair=%s%%7C%s&q=%s&hl=en\"", argv[2], argv[3], argv[4]);
 
 	else if (!(strcmp(argv[1],"-t")))
-		snprintf(command, sizeof(command), "xdg-open \"http://translate.google.com/translate_t#%s|%s|%s\"", argv[2], argv[3], argv[4]);
+		exec("xdg-open \"http://translate.google.com/translate_t#%s|%s|%s\"", argv[2], argv[3], argv[4]);
 
 	else if (!(strcmp(argv[1],"-p")))
-		snprintf(command, sizeof(command), "xdg-open \"http://www.google.com/dictionary/sounds/%s\"", argv[2],".mp3");
+		exec("xdg-open \"http://www.google.com/dictionary/sounds/%s\"", argv[2],".mp3");
 
 	else {
 		printf("Usage: dtool [option] [from-language] [to-language] \"word to find or translate\"\n");
@@ -71,7 +82,5 @@ int main(int argc, char **argv, char **envp) {
 	    exit(0);	
 	}
 
-    system(command);
-	
 	return 0;
 }
