af15c048800c66f859f2b690cf7ee2ab5c4b9341
ribeirodantasdm <ribeirodantasdm@77be1d12-e5d5-11dd-9ba5-89a75009fd5d>
2009-12-29 00:49:35 +0000

    Implementing getopt_long and improvement of the source code

--- dtool.c
+++ dtool.c
@@ -1,86 +1,136 @@
 /*
 ####################################################################
 #                                                                  #
-#  Author:  Marcel Ribeiro Dantas <vuln at slackware-rn.com.br>    #
-#  Website: http://www.slackware-rn.com.br/~vuln                   #
-#  This code is licensed under GNU GPLv3 and any other             #
-#  version of this license.                                        #
+#  Author:  Marcel Ribeiro Dantas <marcel at ribeirodantas.com.br> #
+#  Website: http://www.ribeirodantas.com.br/blog                   #
+#  This code is licensed under GNU GPLv3 or any other newer        #
+#  version of this license released of your choice (GPLv3+).       #
 #                                                                  #
-#  A copy of this license can be read in:                          #
+#  A copy of the license can be read in:                           #
 #  http://www.gnu.org/licenses/gpl-3.0.txt                         #
+#  or in the COPYING file which should follow with this			   #
+#  source file one.												   #	
 #                                                                  #  
 #  dtool - a dictionary tool with many features.                   # 
 #  A tool for help you translating                                 # 
 #  and learning more about a word in                               #  
 #  a lot of different languages                                    #
 #                                                                  #
-#################################################################### */
-
+####################################################################
+*/
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
-#ifdef __linux__
 #include <getopt.h>
-#endif
+#include <stdarg.h>
 #include "config.h"
 
-void exec(const char *fmt, ...)
-{
-    char cmd[8192] = { 0 };
-    va_list l;
+void run(const char* fmt, ...);
+void dictionary(char*, char*, char*);
+void pronunciation(char* );
+void translation(char*, char*, char*);
+void usage(void);
 
-    va_start(l, fmt);
-    /* XXX: I'd use vasprintf, but... gah... */
-    vsnprintf(cmd, 8192, fmt, l);
-    va_end(l);
-    /* XXX: I'll implement something with vfork + execve later */
-    system(cmd);
-    return ;
+char buffer[256] = "0";
+
+int main (int argc, char* argv[]) {
+	char *argument2 = NULL, *argument3 = NULL;
+    int index;
+    int c;
+    if (argc == 1) { 
+		printf("Usage: dtool [from-language] [to-language] [option] \"word or phrase\"\n");
+		printf("For help try: dtool --help\n");
+		exit(0);
+	}
+    while (1) {
+		static struct option long_options[] = {
+			{"dictionary",		required_argument,	0,	'd'},
+			{"pronunciation",	required_argument,	0,	'p'},
+			{"translation",		required_argument,	0,	'T'},
+			{"from",			required_argument, 	0,	'f'},
+			{"to",				required_argument,	0,	't'},
+			{"help",			no_argument,		0,	'h'},
+			{0,					0,					0,	0}
+		};
+		/* getopt_long stores the option index here. */
+		int option_index = 0;
+		c = getopt_long(argc, argv, "d:p:t:f:T:h", long_options, &option_index);
+		/* : to required argument. :: to optional */
+		if (c == -1) { break; }
+    	switch (c) {
+			case 'd':
+				dictionary(optarg, argument2, argument3);
+				break;
+			case 'p':
+				pronunciation(optarg);
+				break;
+			case 'T':
+				translation(optarg, argument2, argument3);
+				break;
+			case 'f':
+				argument2 = optarg;
+				break;
+			case 't':
+				argument3 = optarg;
+				break;
+			case 'h':
+				usage();
+				break;
+			case '?':
+				/* getopt_long already printed an error message. */
+				break;
+			default:
+				abort ();
+		}
+    }
+    for (index = optind; index < argc; index++)
+	printf ("Non-option argument %s\n", argv[index]);
+	return EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv, char **envp) {
-	if (argc == 2 && (!(strcmp(argv[1],"-h")))) {
-		printf("Usage: dtool [option] [from-language] [to-language] \"word to find or translate\"\n");
-		printf("-d        dictionary option.\n");
-		printf("-h        help option.\n");
-		printf("-t        translation option.\n");
-		printf("-p        pronunciation option\n");
-		printf("Special parameter: \n");
-		printf("dtool -p \"word/phrase to listen\"\n");
-	    exit(0);
-    }
-	
-	else if (argc == 3 && (!(strcmp(argv[1],"-p")))) {
-		exec("wget -qc http://www.google.com/dictionary/sounds/%s.mp3", argv[2]);
-		exec("xdg-open %s.mp3 &> /dev/null", argv[2]);
-#ifndef KEEP_FILES
-		exec("rm -f %s.mp3", argv[2]);
-#endif
-    }
+void run(const char* fmt, ...) {
+	va_list l;
+	int flag=0;
 
-	else if (argc != 5 ) {
-		printf("Usage: dtool [option] [from-language] [to-language] \"word to find or translate\"\n");
-		printf("For help try: dtool -h\n");
-	    exit(1);
-    }
-		
-	else if (!(strcmp(argv[1],"-d")))
-        exec("xdg-open \"http://www.google.com/dictionary?aq=f&langpair=%s%%7C%s&q=%s&hl=en\"", argv[2], argv[3], argv[4]);
-
-	else if (!(strcmp(argv[1],"-t")))
-		exec("xdg-open \"http://translate.google.com/translate_t#%s|%s|%s\"", argv[2], argv[3], argv[4]);
-
-	else if (!(strcmp(argv[1],"-p")))
-		exec("xdg-open \"http://www.google.com/dictionary/sounds/%s\"", argv[2],".mp3");
-
-	else {
-		printf("Usage: dtool [option] [from-language] [to-language] \"word to find or translate\"\n");
-		printf("For help try: dtool -h\n");
-	    exit(0);	
+	va_start(l, fmt);
+	vsnprintf(buffer, sizeof(buffer), fmt, l); 
+	va_end(l);
+	flag = system(buffer);
+	if (flag) { 
+		printf("The word you prompted maybe isn't in our data base\n");
+		printf("or you have not created the .dtool directory in your\n");
+		printf("home directory. Do it typing mkdir ~/.dtool\n");
+		exit(1);
 	}
+}
 
-	return 0;
+void dictionary(char* word, char* source, char* destiny) {
+	run("xdg-open \"http://www.google.com/dictionary?aq=f&langpair=%s%%7C%s&q=%s&hl=%s\"",
+	source, source, word, destiny);
+	run("echo %s >> ~/.dtool/.dic", word);
+}
+
+void pronunciation(char* word) {
+	run("wget -nc -qc http://www.google.com/dictionary/sounds/%s.mp3", word);
+	run("mv %s.mp3 ~/.dtool/ &> /dev/null", word);
+	run("ffplay -vn -nodisp ~/.dtool/%s.mp3 &> /dev/null", word);
+	run("echo %s >> ~/.dtool/.pron", word);
+#ifndef KEEP_FILES
+	run("rm -f %s.mp3", word);
+#endif
+}
+
+void translation(char* phrase, char* source, char* destiny) {
+	run("xdg-open \"http://translate.google.com/translate_t#%s|%s|%s\"", source, destiny, phrase);
+}
+
+void usage(void) {
+	printf("Usage: dtool [from-language] [to-language] [option] \"word to find or translate\"\n");
+	printf("--dictionary\t-d\tword\n");
+	printf("--translation\t-T\tword or phrase\n");
+	printf("--pronuncation\t-p\tpronunciation option\n");
+	printf("--from\t\t-f\tfrom what language you want to translate\n");
+	printf("--to\t\t-t\tto what language you want to translate\n");
+	printf("--help\t\t-h\thelp option.\n");
+	exit(0);
 }
