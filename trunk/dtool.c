4109aa8a8b2db3c21841d2b1966559bb75d65b6e
ribeirodantasdm <ribeirodantasdm@77be1d12-e5d5-11dd-9ba5-89a75009fd5d>
2010-01-02 15:36:18 +0000

    Fixing some bugs

--- trunk/dtool.c
+++ trunk/dtool.c
@@ -23,25 +23,27 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <stdarg.h>
+#include <string.h>
 #include "config.h"
 
 void run(const char* fmt, ...);
 void dictionary(const char*, const char*, const char*);
-void pronunciation(const char* );
+void pronunciation(char* );
 void translation(const char*, const char*, const char*);
 void usage(void);
-void dircheck(void);
+void filecheck(const char*);
 
-char buffer[256] = "0";
-char home_dir[64] = "0";
+char buffer[256];	/* run line buffer 		*/
+char dir[64];		/* Working directory 	*/
+char home_dir[64];	/* Home directory		*/
 
 int main (int argc, char* argv[]) {
-	char *argument2 = NULL, *argument3 = NULL;
+	char *from_lang = NULL, *to_lang = NULL;
     int index, c, option_index=0; /* variables to getopt_long */
     if (argc == 1) { 
 		printf("Usage: dtool [from-language] [to-language] [option] \"word or phrase\"\n");
 		printf("For help try: dtool --help\n");
-		exit(0);
+		exit(EXIT_SUCCESS);
 	}
     while (1) {
 		static struct option long_options[] = {
@@ -54,31 +56,44 @@ int main (int argc, char* argv[]) {
 			{0,					0,					0,	0}
 		};
 		c = getopt_long(argc, argv, "d:p:T:f:t:h", long_options, &option_index);
-		if (c == -1) { break; } /* end of argument reading */
+		if (c == -1) { break; } /* end of arguments reading */
 	    switch (c) {
 			case 'd':
-				if ((argument2 == NULL) || (argument3 == NULL)) 
+				if ((from_lang == NULL) && (to_lang == NULL)) 
 					dictionary(optarg, "en", "en");
+				else if ((from_lang == NULL) && (to_lang != NULL))
+					dictionary(optarg, "en", to_lang);
+				else if ((from_lang != NULL) && (to_lang == NULL))
+					dictionary(optarg, from_lang, "en");
 				else	
-					dictionary(optarg, argument2, argument3);
+					dictionary(optarg, from_lang, to_lang);
 				break;
 			case 'p':
 				pronunciation(optarg);
 				break;
 			case 'T':
-				translation(optarg, argument2, argument3);
+				if ((from_lang == NULL) && (to_lang == NULL)) {
+					fprintf(stderr, "Missing argument.\n");
+					usage();
+					exit(EXIT_FAILURE);
+				}
+				else if (((strcmp(from_lang,"en")) != 0) && (to_lang == NULL))
+					translation(optarg, from_lang, "en");
+				else
+					translation(optarg, from_lang, to_lang);
 				break;
 			case 'f':
-				argument2 = optarg;
+				from_lang = optarg;
 				break;
 			case 't':
-				argument3 = optarg;
+				to_lang = optarg;
 				break;
 			case 'h':
 				usage();
+				exit(EXIT_SUCCESS);
 				break;
 			case '?':
-				/* getopt_long, different from getopt already printed an error message. */
+				/* getopt_long, different from getopt, already printed an error message. */
 				break;
 			default:
 				abort ();
@@ -97,60 +112,73 @@ void run(const char* fmt, ...) {
 	vsnprintf(buffer, sizeof(buffer), fmt, l); 
 	va_end(l);
 
-	flag = system(buffer);
+	flag = system(buffer); /* It catchs int returned by the bin execd */
 	if (flag) { 
 		fprintf(stderr, "The word you prompted is not in our data base, or\n");
 		fprintf(stderr, "you should check for your internet connection.\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 }
 
 void dictionary(const char* word, const char* source, const char* destiny) {
-	dircheck();
+	filecheck(".dtool");
 	run("xdg-open \"http://www.google.com/dictionary?aq=f&langpair=%s%%7C%s&q=%s&hl=%s\"",
-	source, destiny, word, destiny); /* ou s s w d? */
+	source, destiny, word, destiny);
 	run("`echo %s - %s/%s >> %s/.dic` &> /dev/null", word, source, destiny, home_dir);
 		/* `` avoid run error msg */
 }
 
-void pronunciation(const char* word) {
-	dircheck();
+void pronunciation(char* word) {
+	filecheck(word);
 	run("wget -nc -qc http://www.google.com/dictionary/sounds/%s.mp3", word);
-	run("mv %s.mp3 %s &> /dev/null", word, home_dir);
-	run("ffplay -vn -nodisp %s/%s.mp3 &> /dev/null", home_dir, word);
-	run("`echo %s >> %s/.pron` &> /dev/null", word, home_dir);
+	run("mv %s.mp3 %s &> /dev/null", word, dir);
+	run("ffplay %s%s.mp3 &> /dev/null", dir, word);
+	run("`echo %s >> %s/.pron` &> /dev/null", word, dir);
 		/* `` show the mv one */
 #ifndef KEEP_FILES
-	run("rm -f %s/%s.mp3", home_dir, word);
+	run("rm -f %s/%s.mp3", dir, word);
 #endif
 }
 
 void translation(const char* phrase, const char* source, const char* destiny) {
-	run("xdg-open \"http://translate.google.com/translate_t#%s|%s|%s\"", source, destiny, phrase);
+	run("xdg-open \"http://translate.google.com/translate_t#%s|%s|%s\"",
+	source, destiny, phrase);
 }
 
 void usage(void) {
-	printf("Usage: dtool [from-language] [to-language] [option] \"word to find or translate\"\n");
+	printf("Usage: dtool [from-language] [to-language] [option] \"word to find or");
+	printf(" translate\"\n");
 	printf("--dictionary\t-d\tword\n");
 	printf("--translation\t-T\tword or phrase\n");
-	printf("--pronuncation\t-p\tpronunciation option\n");
+	printf("--pronunciation\t-p\tpronunciation option\n");
 	printf("--from\t\t-f\tfrom what language you want to translate\n");
 	printf("--to\t\t-t\tto what language you want to translate\n");
 	printf("--help\t\t-h\thelp option.\n");
-	exit(0);
+	printf("Example:\n");
+	printf("user@domn:~$ dtool --from en --to de --translation \"This is an");
+	printf(" useful tool.\"\n");
+	printf("Or even:\n");
+	printf("user@domn:~$ dtool -f en -t de -T \"This is an useful tool.\"\n");
 }
 
-void dircheck() {
+void filecheck(const char* word) {
 	FILE *stream;
 	static int x=1;
 
-	if (x!=0) {
+	if (x!=0) { /* Is it the first time it runs filecheck? */
 		snprintf(home_dir, sizeof(home_dir), "%s/.dtool", getenv("HOME"));
 	}
-	if (!(stream = fopen(home_dir,"r"))) {
-		fprintf(stderr, "The %s/.dtool directory is nonexistent.\n", home_dir);
-		exit(1);
+	if (!(stream = fopen(home_dir,"r"))) { /* Is there the .dtool directory ? */
+		fprintf(stderr, "The %s/ directory is nonexistent.\n", home_dir);
+		fclose(stream);
+		exit(EXIT_FAILURE);
 	}
-	fclose(stream);
-	x=0;
+	snprintf(dir, sizeof(dir), "%s/%s.mp3", home_dir, word); 
+	if (((strcmp(word,".dtool")) != 0) && (stream = fopen(dir,"r"))) {
+					/* You've already downloaded it */
+		run("ffplay %s &> /dev/null", dir);
+		fclose(stream);
+		exit(EXIT_SUCCESS);
+	}
+	x=0; /* Making sure snprintf won't overwrite home_dir repeatedly */
 }
